c98115001171ad84753734e3cceffe37c207b522
Tjark Miener <tjark.miener4692@googlemail.com>
2021-09-20 18:07:50 +0200

    support MARS melibea files for plotting scripts

--- pymars/plotRES.py
+++ pymars/plotRES.py
@@ -38,7 +38,10 @@ import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.coordinates import Angle
 import ctaplot
+import uproot
 
 def main():
 
@@ -53,6 +56,15 @@ def main():
     parser.add_argument('--output_dir', '-o',
                         help='path where to save generated files. By default, the current directory is used.',
                         default="./")
+    parser.add_argument('--hadronnessCut', '-k',
+                        help='maximum hadronness values',
+                        default=None,
+                        type=float)
+    parser.add_argument('--sizeCut', '-s',
+                        help='minimum size values',
+                        default=None,
+                        nargs='+',
+                        type=float)
     parser.add_argument('--angular', '-a',
                         dest='angular',
                         help='plot angular resolution',
@@ -80,12 +92,56 @@ def main():
         for file in files:
             if file.split('.')[-1] == "h5":
                 data = pd.HDFStore(file, 'r')
-                mc_energy = np.array(data['gamma']['mc_energy'])
-                reco_energy = np.array(data['gamma']['reco_energy'])
-                ax = ctaplot.plot_energy_resolution(mc_energy, reco_energy, label=f"{file.split('/')[-1]}")
+                mc_energy = np.array(data['gamma']['mc_energy']) * u.TeV
+                reco_energy = np.array(data['gamma']['reco_energy']) * u.TeV
+                print(len(reco_energy))
+
+                if args.hadronnessCut or args.sizeCut:
+                    parameters = np.dstack(data['gamma']["parameters"].to_numpy())
+                    hadronness = 1 - data['gamma']["reco_gammaness"].to_numpy()
+                    if args.sizeCut:
+                        #TODO: Make generic for any given number of telescopes
+                        size_m1 = parameters[0][0]
+                        size_m2 = parameters[1][0]
+                        sizeMask = (size_m1 > args.sizeCut[0]) & (size_m2 > args.sizeCut[1])
+                        mc_energy = mc_energy[sizeMask]
+                        reco_energy = reco_energy[sizeMask]
+                        hadronness = hadronness[sizeMask]
+
+                    if args.hadronnessCut:
+                        hadronnessMask = (hadronness < args.hadronnessCut)
+                        mc_energy = mc_energy[hadronnessMask]
+                        reco_energy = reco_energy[hadronnessMask]
+
+                    print(len(reco_energy))
+                #ax = ctaplot.plot_energy_resolution(mc_energy, reco_energy, label=f"{file.split('/')[-1]}")
             elif file.split('.')[-1] == "root":
                 print("root")
-                
+                melibea_file = uproot.open(file)
+                evts = melibea_file["Events"]
+                mc_energy =  np.asarray(evts["MMcEvt_1.fEnergy"].array()) * u.GeV
+                reco_energy = np.asarray(evts["MEnergyEst.fEnergy"].array()) * u.GeV
+                print(len(reco_energy))
+
+                if args.hadronnessCut or args.sizeCut:
+                    hadronness = np.asarray(evts["MHadronness.fHadronness"].array())
+                    if args.sizeCut:
+                        size_m1 = np.asarray(evts["MHillas_1.fSize"].array())
+                        size_m2 = np.asarray(evts["MHillas_2.fSize"].array())
+                        sizeMask = (size_m1 > args.sizeCut[0]) & (size_m2 > args.sizeCut[1])
+                        mc_energy = mc_energy[sizeMask]
+                        reco_energy = reco_energy[sizeMask]
+                        hadronness = hadronness[sizeMask]
+
+                    if args.hadronnessCut:
+                        hadronnessMask = (hadronness < args.hadronnessCut)
+                        mc_energy = mc_energy[hadronnessMask]
+                        reco_energy = reco_energy[hadronnessMask]
+
+                    print(len(reco_energy))
+
+            ax = ctaplot.plot_energy_resolution(mc_energy.to(u.TeV).value, reco_energy.to(u.TeV).value, label=f"{file.split('/')[-1]}")            
+
         if args.aleksic:
             # Aleksic et al. (2015) MAGIC performance (energy)
             aleksic_energy = np.array([60.14412430231677, 88.92924106247686, 146.42158223581913, 378.0025174064791, 598.5081431652516, 1500.4475087782453, 2329.7193634991513, 3761.5908025786603, 5955.893474058259, 9430.230170174897, 14786.02308938286])*1e-3
@@ -118,18 +174,104 @@ def main():
     
         for file in files:
             if file.split('.')[-1] == "h5":
-
                 data = pd.HDFStore(file, 'r')
-                reco_energy = np.array(data['gamma']['reco_energy'])
-                mc_alt = np.array(data['gamma']['mc_altitude'])
-                reco_alt = np.array(data['gamma']['reco_altitude'])
-                mc_az = np.array(data['gamma']['mc_azimuth'])
-                reco_az = np.array(data['gamma']['reco_azimuth'])
+                reco_energy = np.array(data['gamma']['reco_energy']) * u.TeV
+                mc_alt = np.array(data['gamma']['mc_altitude']) * u.rad
+                reco_alt = np.array(data['gamma']['reco_altitude']) * u.rad
+                mc_az = np.array(data['gamma']['mc_azimuth']) * u.rad
+                reco_az = np.array(data['gamma']['reco_azimuth']) * u.rad
+                if args.hadronnessCut or args.sizeCut:
+                    parameters = np.dstack(data['gamma']["parameters"].to_numpy())
+                    hadronness = 1 - data['gamma']["reco_gammaness"].to_numpy()
+                    if args.sizeCut:
+                        #TODO: Make generic for any given number of telescopes
+                        size_m1 = parameters[0][0]
+                        size_m2 = parameters[1][0]
+                        sizeMask = (size_m1 > args.sizeCut[0]) & (size_m2 > args.sizeCut[1])
+                        reco_energy = reco_energy[sizeMask]
+                        mc_alt = mc_alt[sizeMask]
+                        reco_alt = reco_alt[sizeMask]
+                        mc_az = mc_az[sizeMask]
+                        reco_az = reco_az[sizeMask]
 
-                ax = ctaplot.plot_angular_resolution_per_energy(reco_alt, reco_az, mc_alt, mc_az, reco_energy, bias_correction=False, label=f"{file.split('/')[-1]}")
+                        hadronness = hadronness[sizeMask]
+
+                    if args.hadronnessCut:
+                        hadronnessMask = (hadronness < args.hadronnessCut)
+                        reco_energy = reco_energy[hadronnessMask]
+                        mc_alt = mc_alt[hadronnessMask]
+                        reco_alt = reco_alt[hadronnessMask]
+                        mc_az = mc_az[hadronnessMask]
+                        reco_az = reco_az[hadronnessMask]
+
+                    print(len(reco_energy))
+
+                #ax = ctaplot.plot_angular_resolution_per_energy(reco_alt, reco_az, mc_alt, mc_az, reco_energy, bias_correction=False, label=f"{file.split('/')[-1]}")
             
             elif file.split('.')[-1] == "root":
                 print("root")
+                melibea_file = uproot.open(file)
+                evts = melibea_file["Events"]
+                reco_energy = np.asarray(evts["MEnergyEst.fEnergy"].array()) * u.GeV
+                #mc_alt= np.asarray(evts["MMcEvt_1.fTheta"].array()) * u.deg
+                #reco_alt = (90.0-np.asarray(evts["MStereoPar.fDirectionZd"].array())) * u.deg
+                #mc_az = np.asarray(evts["MMcEvt_1.fPhi"].array()) * u.deg
+                #reco_az = np.asarray(evts["MStereoPar.fDirectionAz"].array()) * u.deg
+
+                #src_pos_cam_Y = np.asarray(evts["MSrcPosCam_1.fY"].array())
+                #src_pos_cam_X = np.asarray(evts["MSrcPosCam_1.fX"].array())
+
+                reco_alt =  (np.asarray(evts["MStereoParDisp.fDirectionY"].array()) * 0.00337 +  90.0 - np.asarray(evts["MPointingPos_1.fZd"].array())) * u.deg
+                reco_az =  (np.asarray(evts["MStereoParDisp.fDirectionX"].array()) * 0.00337 +  np.asarray(evts["MPointingPos_1.fAz"].array())) * u.deg
+
+                mc_alt =  (np.asarray(evts["MSrcPosCam_1.fY"].array()) * 0.00337 +  90.0 - np.asarray(evts["MPointingPos_1.fZd"].array())) * u.deg
+                mc_az =  (np.asarray(evts["MSrcPosCam_1.fX"].array()) * 0.00337 +  np.asarray(evts["MPointingPos_1.fAz"].array())) * u.deg
+
+                reco_alt =  (np.asarray(evts["MStereoParDisp.fDirectionY"].array()) * 0.00337) * u.deg
+                reco_az =  (np.asarray(evts["MStereoParDisp.fDirectionX"].array()) * 0.00337) * u.deg
+
+                mc_alt =  (np.asarray(evts["MSrcPosCam_1.fY"].array()) * 0.00337) * u.deg
+                mc_az =  (np.asarray(evts["MSrcPosCam_1.fX"].array()) * 0.00337) * u.deg 
+
+                print("lalaa")
+                print(mc_alt)
+                print(reco_alt)
+                print(mc_az)
+                print(reco_az)
+                #mc_az = Angle(np.deg2rad(src_pos_cam_X * 0.00337), u.rad)
+                # Adding the array pointing in the pointing container
+                #data.pointing.array_altitude = u.Quantity(np.deg2rad(90.0 - pointing_altitude[i]), u.rad)
+                #data.pointing.array_azimuth = u.Quantity(np.deg2rad(pointing_azimuth[i]), u.rad)
+                #pointing_alt = 90.0-np.asarray(evts["MPointingPos_1.fZd"].array())
+                #pointing_az = np.asarray(evts["MPointingPos_1.fAz"].array())
+
+                print(len(reco_energy))
+
+                if args.hadronnessCut or args.sizeCut:
+                    hadronness = np.asarray(evts["MHadronness.fHadronness"].array())
+                    if args.sizeCut:
+                        size_m1 = np.asarray(evts["MHillas_1.fSize"].array())
+                        size_m2 = np.asarray(evts["MHillas_2.fSize"].array())
+                        sizeMask = (size_m1 > args.sizeCut[0]) & (size_m2 > args.sizeCut[1])
+                        reco_energy = reco_energy[sizeMask]
+                        hadronness = hadronness[sizeMask]
+                        mc_alt = mc_alt[sizeMask]
+                        reco_alt = reco_alt[sizeMask]
+                        mc_az = mc_az[sizeMask]
+                        reco_az = reco_az[sizeMask]
+
+                    if args.hadronnessCut:
+                        hadronnessMask = (hadronness < args.hadronnessCut)
+                        mc_energy = mc_energy[hadronnessMask]
+                        reco_energy = reco_energy[hadronnessMask]
+                        mc_alt = mc_alt[hadronnessMask]
+                        reco_alt = reco_alt[hadronnessMask]
+                        mc_az = mc_az[hadronnessMask]
+                        reco_az = reco_az[hadronnessMask]
+                    print(len(reco_energy))
+
+
+            ax = ctaplot.plot_angular_resolution_per_energy(reco_alt.to(u.rad).value, reco_az.to(u.rad).value, mc_alt.to(u.rad).value, mc_az.to(u.rad).value, reco_energy.to(u.TeV).value, bias_correction=False, label=f"{file.split('/')[-1]}")
 
             
         if args.aleksic:
@@ -142,8 +284,8 @@ def main():
         # Scale, labels and title
         ax.set_ylabel(r'$\theta [deg]$',fontsize=25)
 
-        ax.set_ybound(0.04,0.25)
-        ax.set_yticks([0.05,0.1,0.15,0.2,0.25])
+        #ax.set_ybound(0.04,0.25)
+        #ax.set_yticks([0.05,0.1,0.15,0.2,0.25])
 
         ax.set_xlabel("E_reco [TeV]",fontsize=25)
         ax.set_xscale('log')
--- pymars/plotROC.py
+++ pymars/plotROC.py
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #/* ================================================================================ *\
 #!
@@ -28,7 +29,7 @@
 #   plotROC - Python ctaplot-based program to plot ROC curves
 #             from MARS melibea (under development) and CTLearn files.
 #
-#   For help: $ python plotROC.py -h
+#   For help: $ pymars-plotROC -h
 #
 #######################################################################################
 
@@ -40,6 +41,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import ctaplot
+import uproot
 
 def main():
 
@@ -51,6 +53,12 @@ def main():
     parser.add_argument('--pattern', '-p',
                         help='pattern to mask unwanted files',
                         default="*")
+    parser.add_argument('--input_offdata', '-j',
+                        help='input directory for the off data (melibea)',
+                        default=None)
+    parser.add_argument('--pattern_offdata', '-q',
+                        help='pattern to mask unwanted files for the off data input directory',
+                        default="*")
     parser.add_argument('--output_dir', '-o',
                         help='path where to save generated files. By default, the current directory is used.',
                         default="./")
@@ -60,31 +68,84 @@ def main():
     # Input handling
     abs_file_dir = os.path.abspath(args.input_dir)
     files = glob.glob(os.path.join(abs_file_dir, args.pattern))
-    
+    if args.input_offdata:
+        abs_file_dir_off = os.path.abspath(args.input_offdata)
+        off_files = glob.glob(os.path.join(abs_file_dir_off, args.pattern_offdata))
+
+
     plt.figure(figsize=(12,12))
     ax = plt.axes()
+
+    # color cycle of matplotlib
+    color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
     
-    for file in files:
+    for i, file in enumerate(files):
         if file.split('.')[-1] == "h5":
 
             data = pd.HDFStore(file, 'r')
+            linestyle = {'proton': 'dashed', 'off': 'solid'}
             for bkg in ['proton', 'off']:
                 bkg_mc_particle = np.array(data[bkg]['mc_particle']).astype(int)
-                bkg_reco_particle = np.array(data[bkg]['reco_particle']).astype(int)
                 bkg_reco_gammaness = np.array(data[bkg]['reco_gammaness']).astype(float)
 
                 gamma_mc_particle = np.array(data['gamma']['mc_particle']).astype(int)
-                gamma_reco_particle = np.array(data['gamma']['reco_particle']).astype(int)
                 gamma_reco_gammaness = np.array(data['gamma']['reco_gammaness']).astype(float)
                 
                 # Concatenate the background with the gammas (ringwobble)
                 mc_particle = np.concatenate((bkg_mc_particle, gamma_mc_particle))
-                reco_particle = np.concatenate((bkg_reco_particle, gamma_reco_particle))
                 reco_gammaness = np.concatenate((bkg_reco_gammaness, gamma_reco_gammaness))
                 
                 fpr, tpr, thresholds = metrics.roc_curve(mc_particle, reco_gammaness)
-                            
-                ax = ctaplot.plot_roc_curve(mc_particle, reco_gammaness, label="{} (vs {}), AUC={:0.3f}".format(file.split('/')[-1], bkg, metrics.auc(fpr, tpr)), linewidth=1.5)
+                
+                ax = ctaplot.plot_roc_curve(mc_particle, reco_gammaness, label="{} (vs {}), AUC={:0.3f}".format(file.split('/')[-1], bkg, metrics.auc(fpr, tpr)), color=color_cycle[i], linestyle=linestyle[bkg], linewidth=1.5)
+
+        elif file.split('.')[-1] == "root":
+            melibea_file = uproot.open(file)
+            evts = melibea_file["Events"]
+            if b'MMcEvt_1.' in evts.keys():
+                if int(evts["MMcEvt_1.fPartId"].array()[0]) == 1:
+                    gamma_mc_particle_root = np.asarray(evts["MMcEvt_1.fPartId"].array())
+                    gamma_reco_gammaness_root = 1.0 - np.asarray(evts["MHadronness.fHadronness"].array())
+
+    for file in files:
+        if file.split('.')[-1] == "root":
+            melibea_file = uproot.open(file)
+            evts = melibea_file["Events"]
+            if b'MMcEvt_1.' not in evts.keys(): continue
+            if int(evts["MMcEvt_1.fPartId"].array()[0]) == 1: continue
+
+            bkg_reco_gammaness = 1.0 - np.asarray(evts["MHadronness.fHadronness"].array())
+            bkg_mc_particle = np.zeros(len(bkg_reco_gammaness))
+
+            # Concatenate the background with the gammas (ringwobble) from MARS
+            mc_particle = np.concatenate((bkg_mc_particle, gamma_mc_particle_root))
+            reco_gammaness = np.concatenate((bkg_reco_gammaness, gamma_reco_gammaness_root))
+
+            fpr, tpr, thresholds = metrics.roc_curve(mc_particle, reco_gammaness)
+
+            ax = ctaplot.plot_roc_curve(mc_particle, reco_gammaness, label="ST0310-MARS (vs MCs proton), AUC={:0.3f}".format(metrics.auc(fpr, tpr)), color='black', linestyle="dashed", linewidth=1.5)
+
+    if args.input_offdata:
+        bkg_mc_particle = []
+        bkg_reco_gammaness = []
+        for file in off_files:
+            if file.split('.')[-1] == "root":
+                melibea_file = uproot.open(file)
+                evts = melibea_file["Events"]
+                if b'MMcEvt_1.' in evts.keys(): continue
+                file_reco_gammaness = 1.0 - np.asarray(evts["MHadronness.fHadronness"].array())
+                file_mc_particle = np.zeros(len(file_reco_gammaness))
+                # Concatenate the background with the gammas (ringwobble)
+                bkg_mc_particle = np.concatenate((bkg_mc_particle, file_mc_particle))
+                bkg_reco_gammaness = np.concatenate((bkg_reco_gammaness, file_reco_gammaness))
+
+        # Concatenate the background with the gammas (ringwobble) from MARS
+        mc_particle = np.concatenate((bkg_mc_particle, gamma_mc_particle_root))
+        reco_gammaness = np.concatenate((bkg_reco_gammaness, gamma_reco_gammaness_root))
+
+        fpr, tpr, thresholds = metrics.roc_curve(mc_particle, reco_gammaness)
+
+        ax = ctaplot.plot_roc_curve(mc_particle, reco_gammaness, label="ST0310-MARS (vs off data), AUC={:0.3f}".format(metrics.auc(fpr, tpr)), color='black', linestyle="solid", linewidth=1.5)
 
     ax.set_xlabel('False positive rate', fontsize=25)
     plt.xlim(0.0,1.0)
